4497467925da20982ef7cfe11fe8e040e39c7a4e
Jordan Wolter <jordan.wolter@icloud.com>
2022-09-17 11:30:05 -0500

    finished question 5

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -42,7 +42,7 @@ console.log( me.favThreeFoods[0], me.favThreeFoods[2] );
   - Console.log your updated shoe count. 
 */
 console.log( me.shoeCount );
-//me.shoeCount += 1;
+
 console.log( me.shoeCount += 1 );
 
 /* 5. Add a new property to an existing object.
@@ -51,3 +51,7 @@ console.log( me.shoeCount += 1 );
   - Cool huh? It works, even though it wasn't there before
   - Console.log your object
 */
+
+me.favoriteColor = 'red';
+
+console.log( me.favoriteColor )
